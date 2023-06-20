afbc72629c6b82629e6aa2d4fd15418092e0aa10
1GR3 <ivo.gregurec@gmail.com>
2023-06-20 12:01:25 +0200

    responsive header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,10 +6,12 @@ function Header() {
   return (
     <header className="container sticky-top">
       <div className="row">
-        <div className="col text-start">
-          <Logo fill="red" />
+        <div className="col text-center text-md-start">
+          <a href="/">
+            <Logo fill="" />
+          </a>
         </div>
-        <div className="col text-end ">
+        <div className="col text-end d-none d-md-block">
           <QRcode className="qr-code" />
         </div>
       </div>
