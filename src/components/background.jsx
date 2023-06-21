05f2da2f359ffdaee88c68d798462bdeca3a4b0e
1GR3 <ivo.gregurec@gmail.com>
2023-06-21 21:55:06 +0200

    backgroud changing colors on scroll

--- src/components/background.jsx
+++ src/components/background.jsx
@@ -1,6 +1,28 @@
-import * as React from "react";
+import React from "react";
+import { useSpring, animated } from "@react-spring/web";
 
-const Background = ({ state }) => {
-  return <div className={"background state" + state}></div>;
+const Background = ({ scrollYProgress }) => {
+  const gradientDefinition = [
+    { color: "#27a7b8", position: 0 },
+    { color: "#27a7b8", position: 0.15 },
+    { color: "#fff", position: 0.25 },
+    { color: "#fff", position: 0.75 },
+    { color: "#111", position: 0.85 },
+    { color: "#111", position: 1 },
+  ];
+
+  const interpolateGradient = (gradientDefinition) => {
+    return scrollYProgress.to(
+      gradientDefinition.map((stop) => stop.position),
+      gradientDefinition.map((stop) => stop.color)
+    );
+  };
+
+  const springProps = useSpring({
+    background: interpolateGradient(gradientDefinition),
+  });
+
+  return <animated.div className="background" style={springProps} />;
 };
+
 export default Background;
