5efebb21ccafd5309cd64a278bdb56a5028e427d
1GR3 <ivo.gregurec@gmail.com>
2023-06-22 17:12:28 +0200

    phone sizing animation

--- src/components/phone.jsx
+++ src/components/phone.jsx
@@ -1,6 +1,26 @@
 import * as React from "react";
+import { useSpring, animated, config } from "@react-spring/web";
 
-const Phone = ({ state }) => {
-  return <div className={"phone state" + state}></div>;
+const Phone = ({ scrollYProgress }) => {
+  const phoneSize = scrollYProgress.interpolate([0, 0.13, 1], [142, 100, 100]);
+  const phonePosition = scrollYProgress.interpolate(
+    [0, 0.13, 1],
+    ["-25%", "0%", "0%"]
+  );
+  const phoneProps = useSpring({
+    transform: phoneSize.to((value) => `scale(${value}%) `),
+    bottom: phonePosition,
+    config: {
+      immediate: true,
+    },
+  });
+  return (
+    <div className="phone-holder">
+      <animated.div
+        className={"phone state0"}
+        style={phoneProps}
+      ></animated.div>
+    </div>
+  );
 };
 export default Phone;
