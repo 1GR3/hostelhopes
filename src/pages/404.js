0883525517f5f44fc5a75b260e0f03b16b84ae33
1GR3 <ivo.gregurec@gmail.com>
2023-06-23 16:01:30 +0200

    404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,49 +1,53 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import * as React from "react";
+import { Link } from "gatsby";
+import Header from "../components/header";
 
 const pageStyles = {
   color: "#232129",
   padding: "96px",
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
+};
 const headingStyles = {
   marginTop: 0,
   marginBottom: 64,
   maxWidth: 320,
-}
+};
 
 const paragraphStyles = {
   marginBottom: 48,
-}
+};
 const codeStyles = {
   color: "#8A6534",
   padding: 4,
   backgroundColor: "#FFF4DB",
   fontSize: "1.25rem",
   borderRadius: 4,
-}
+};
 
 const NotFoundPage = () => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry 😔, we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
+    <main className=" text-center bg-hero">
+      <section className="container dark">
+        <Header />
+        <div className="d-flex align-items-center justify-content-center py-5">
+          <div className="d-block">
+            <p className="lead">The quote of the day No. 404:</p>
+            <h1>Not All Those Who Wander Are Lost</h1>
+            <p className="lead">
+              ...but you are...
+              <br />
+              <br />
+              <Link to="/" className="btn btn-light">
+                Go home
+              </Link>
+            </p>
+          </div>
+        </div>
+      </section>
     </main>
-  )
-}
+  );
+};
 
-export default NotFoundPage
+export default NotFoundPage;
 
-export const Head = () => <title>Not found</title>
+export const Head = () => <title>Not found</title>;
