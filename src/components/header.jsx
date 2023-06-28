5cee6f838f8ecd211ebc613d3e8076f831e61a99
1GR3 <ivo.gregurec@gmail.com>
2023-06-28 16:09:58 +0200

    header rearanged for the new  hoverable qr menu component

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,8 @@
 import * as React from "react";
-import { ReactComponent as Logo } from "./logo.svg";
-import { ReactComponent as QRcode } from "./QRcode.svg";
+import { ReactComponent as Logo } from "../images/logo.svg";
+import { ReactComponent as QRcode } from "../images/QRcode.svg";
+import { ReactComponent as AppleO } from "../images/apple-o.svg";
+import { ReactComponent as PlayO } from "../images/play-o.svg";
 import { useSpring, animated, config } from "@react-spring/web";
 
 const AnimatedLogo = animated(Logo);
@@ -33,7 +35,39 @@ const Header = ({ landingpage, light = false, scrollYProgress }) => {
           </a>
         </div>
         <div className="col text-end d-none d-lg-block">
-          <QRcode className="qr-code" />
+          <div className="qr-code">
+            <QRcode className="qr" />
+            <div className="details">
+              <div className="scan">
+                Scan with your phone
+                <br /> to download
+              </div>
+              <a href="" className="btn btn-outline-success d-block mb-2">
+                <div className="row mx-auto">
+                  <div className="col-4 p-0 justify-content-center">
+                    <AppleO />
+                  </div>
+                  <div className="col p-0 text-start">
+                    <small>Download on the</small>
+                    <br />
+                    App Store
+                  </div>
+                </div>
+              </a>
+              <a href="" className="btn btn-outline-success d-block mb-2">
+                <div className="row mx-auto">
+                  <div className="col-4 p-0 justify-content-center">
+                    <PlayO />
+                  </div>
+                  <div className="col p-0 text-start">
+                    <small>Get it on</small>
+                    <br />
+                    Google Play
+                  </div>
+                </div>
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </header>
